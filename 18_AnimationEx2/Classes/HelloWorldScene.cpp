f0e2f0d46e8a153028547bbdc71bbf246e962dff
YeonjaeLee(yeonj_000) <yeonjae115@gmail.com>
2017-04-20 15:03:00 +0900

    CH09: Animation

---
+++ 18_AnimationEx2/Classes/HelloWorldScene.cpp
@@ -0,0 +1,71 @@
+﻿#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = HelloWorld::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool HelloWorld::init()
+{
+	if (!LayerColor::initWithColor(Color4B(255, 255, 255, 255)))
+	{
+		return false;
+	}
+	/////////////////////////////////////////////
+
+	// 다양한 방법으로 스프라이트시트 텍스처를 만든다.
+
+	// --------------------------------------------------------------------
+	// Type 1 : from Sprite
+	// --------------------------------------------------------------------
+	auto sprite = Sprite::create("Images/grossini_dance_atlas.png");
+	auto texture1 = sprite->getTexture();
+
+	// --------------------------------------------------------------------
+	// Type 2 : from Texture
+	// --------------------------------------------------------------------
+	auto texture2 = Director::getInstance()->getTextureCache()->addImage("Images/grossini_dance_arlas.png");
+
+	// --------------------------------------------------------------------
+	// Type 3 : from BatchNode
+	// --------------------------------------------------------------------
+
+	// small capacity. Testing resizing.
+	// Don't use capacity=1 in your real game. It is expensive to resize the capacity
+	auto batch = SpriteBatchNode::create("Images/grossini_dance_atlas.png", 10);
+	auto texture3 = batch->getTexture();
+	// --------------------------------------------------------------------
+
+
+	auto animation = Animation::create();
+	animation->setDelayPerUnit(0.3f);
+
+	for (int i = 0; i < 14; i++)
+	{
+		// 첫째 줄에 5개의 프레임이 있고, 6번째부터는 두 번째 줄에 있으므로
+		// 6번째 (idx==5)부터는 y 좌표의 값을 증가시켜야 한다.
+		int column = i % 5;
+		int row = i / 5;
+
+		animation->addSpriteFrameWithTexture(
+			texture3,
+			Rect(column * 85, row * 121, 85, 121));
+	}
+
+	// 스프라이트 생성 및 초기화
+	auto pMan = Sprite::createWithTexture(texture3, Rect(0, 0, 85, 121));
+	pMan->setPosition(Vec2(240, 160));
+	this->addChild(pMan);
+
+	// 애니메이션 액션 실행
+	auto animate = Animate::create(animation);
+	auto rep = RepeatForever::create(animate);
+	pMan->runAction(rep);
+		
+	return true;
+}
---
+++ 21_AnimationEx5/Classes/HelloWorldScene.cpp
@@ -0,0 +1,46 @@
+﻿#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = HelloWorld::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool HelloWorld::init()
+{
+	if (!LayerColor::initWithColor(Color4B(255, 255, 255, 255)))
+	{
+		return false;
+	}
+	/////////////////////////////////////////////
+
+	// 배치노드는 별도의 TextureCache를 지정하는 일 없이 자기 것을 자기가 직접 가지고 있다.
+	// 이미 자기의 텍스처이므로 해당 텍스처 안의 이미지는 얼마든지 사용해도
+	// 배치노드 안에서는 결국 하나로 처리한다.
+
+	auto BatchNode = SpriteBatchNode::create("Images/grossini_dance_atlas.png", 50);
+	this->addChild(BatchNode, 0, 1);
+
+	auto texture = BatchNode->getTexture();
+
+	for (int i = 0; i < 14; i++)
+	{
+		// 첫째 줄에 5개의 프레임이 있고, 6번째부터는 두 번째 줄에 있으므로
+		// 6번째 (idx==5)부터는 y좌표의 값을 증가시켜야 한다.
+		int column = i % 5;
+		int row = i / 5;
+
+		auto pTemp = Sprite::createWithTexture(
+			texture,
+			Rect(column * 85, row * 121, 85, 121));
+		pTemp->setPosition(Vec2(column * 85, row * 121));
+
+		BatchNode->addChild(pTemp);
+	}
+
+	return true;
+}
