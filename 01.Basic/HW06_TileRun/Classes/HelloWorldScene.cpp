f91e957eb1aceabfda3ae2e8c608953902776689
YeonjaeLee(yeonj_000) <yeonjae115@gmail.com>
2017-04-27 09:11:41 +0900

    HW: TileRun, WomanCliff

---
+++ 01.Basic/HW06_TileRun/Classes/HelloWorldScene.cpp
@@ -0,0 +1,89 @@
+﻿#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = HelloWorld::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool HelloWorld::init()
+{
+	if (!LayerColor::initWithColor(Color4B(255, 255, 255, 255)))
+	{
+		return false;
+	}
+	/////////////////////////////////////////////
+	winSize = Director::getInstance()->getWinSize();
+
+	tmap = TMXTiledMap::create("TileMaps/TileRun3.tmx");
+	background = tmap->getLayer("Background");
+	metainfo = tmap->getLayer("MetaInfo");
+	metainfo->setVisible(false);
+	this->addChild(tmap, 0, 11);
+
+	TMXObjectGroup* objects = tmap->getObjectGroup("Objects");
+	ValueMap spawnPoint = objects->getObject("SpawnPoint");
+
+	int x = spawnPoint["x"].asInt();
+	int y = spawnPoint["y"].asInt();
+
+	womanPosition = Vec2(x, y);
+	this->createWoman();
+	
+	this->schedule(schedule_selector(HelloWorld::myTick), 1.0f / 60);
+
+	return true;
+}
+
+void HelloWorld::createWoman()
+{
+	woman = Sprite::create("Images/grossinis_sister1.png");
+	woman->setPosition(womanPosition);
+	woman->setScale(0.5);
+	this->addChild(woman);
+}
+// 현재 탭으로 선택된 타일의 위치를 가져온다.
+Vec2 HelloWorld::tileCoordForPosition(Vec2 position)
+{
+	int x = position.x / tmap->getTileSize().width;
+	int y = ((tmap->getMapSize().height * tmap->getTileSize().height) - position.y) / tmap->getTileSize().height;
+	return Vec2(x, y);
+}
+
+void HelloWorld::setPlayerPosition(Vec2 position)
+{
+	// 탭된 위치 구하기
+	Vec2 tileCoord = this->tileCoordForPosition(position);
+	// 현재 위치의 Tile GID 구하기
+	int tileGid = this->metainfo->getTileGIDAt(tileCoord);
+	if (tileGid)
+	{
+		// 타일맵에서 GID에 해당하는 부분의 속성 읽어 오기
+		Value& properties = tmap->getPropertiesForGID(tileGid);
+
+		if (!properties.isNull())
+		{
+			std::string cliff = properties.asValueMap()["Cliff"].asString();
+			if (cliff == "YES")
+			{
+				log("낭떨어지...");
+				return;
+			}
+		}
+	}
+}
+
+void HelloWorld::myTick(float f)
+{
+	tmap->setPosition(Vec2(0 + i, 0));
+	this->setPlayerPosition(Vec2(womanPosition.x - i, womanPosition.y));
+	i--;
+	if (i == -704)
+	{
+		i = 0;
+	}
+}
---
+++ 01.Basic/HW06_TileRun/Classes/HelloWorldScene.h
@@ -0,0 +1,37 @@
+﻿#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+#pragma execution_character_set("utf-8")
+#endif
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::LayerColor
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	CREATE_FUNC(HelloWorld);
+
+	cocos2d::Size		winSize;
+	cocos2d::Vec2		womanPosition;
+	cocos2d::Sprite*	woman;
+
+	cocos2d::TMXTiledMap* tmap;
+	cocos2d::TMXLayer*	background;
+	cocos2d::TMXLayer*	metainfo;
+
+	void createWoman();
+	
+	cocos2d::Vec2 tileCoordForPosition(cocos2d::Vec2 position);
+	void setPlayerPosition(cocos2d::Vec2 position);
+
+	void myTick(float f);
+	int i = 0;
+
+};
+
+#endif // __HELLOWORLD_SCENE_H__
